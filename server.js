821d4df462667d2adcf23ffee9fb231cfc468eb1
Nicolas <nsabre@kerlan-info.fr>
2021-07-22 10:47:00 +0200

    add method for get products, categories, moralEntities, add moralEntitie

--- server.js
+++ server.js
@@ -36,11 +36,65 @@ app.get("/moralEntities", (request, response) => {
     });
 });
 
+//create MoralEntities
+//?Name=c&Address=d&Code=f&UnitPrice=g
+// ATTENION Unit Price doit contenir un . pour les décimales
+app.put("/moralEntitie", (request, response) => {
+    const req=request.query
+    const query="INSERT INTO moralentities_new SET ?";
+    var CURRENT_TIMESTAMP = mysql.raw('now()');
+    const params={CreateDate:CURRENT_TIMESTAMP,LastModifiedDate:CURRENT_TIMESTAMP,Name:req.Name,Address:req.Address,Enabled:1,Code:req.Code,UnitPrice:req.UnitPrice}
+    connection.query(query,params,(err,result,fields) => {
+        if(err) throw err;
+        response.json("Création du client OK");
+    });
+});
+
+//get One MoralEntitie
+app.get("/moralEntitie/:id", (request, response) => {
+    const req=request.query
+    connection.query('SELECT * FROM moralentities_new WHERE Id = '+request.params.id, (err,data) => {
+      if(err) throw err;
+      response.json({data})
+    
+    });
+});
+
+//get ALL Categories
+app.get("/Categories", (request, response) => {
+    const req=request.query
+    connection.query('SELECT * FROM categories_new', (err,data) => {
+      if(err) throw err;
+      response.json({data})
+    
+    });
+});
+
+//Get Catégories filles d'une catégorie mère
+app.get("/Categories/:ParentId", (request, response) => {
+    const req=request.query
+    connection.query('SELECT * FROM categories_new WHERE ParentId = '+ request.params.ParentId, (err,data) => {
+      if(err) throw err;
+      response.json({data})
+    
+    });
+});
+
+//get ALL Products
+app.get("/Products", (request, response) => {
+    const req=request.query
+    connection.query('SELECT * FROM products_new', (err,data) => {
+      if(err) throw err;
+      response.json({data})
+    
+    });
+});
+
 //TODO : 
-//-création MoralEntities
+//-création MoralEntities ==> OK
 //-modification MoralEntities (code Inovex + Prix)
 //-création catégories
 //-création products
 //-saisie fin de mois
 //-saisie tonnage (measures)
-//-création formualaire de saisie (comment stocker cela en BDD ???)
+//-création formualaire de saisie (comment stocker cela en BDD ??? ===> création d'une table tampon pour stocker les champs a mettre dans le formulaire)
